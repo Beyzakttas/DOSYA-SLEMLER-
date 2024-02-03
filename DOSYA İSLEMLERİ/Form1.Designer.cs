f2a9b716903b424bff5840fc2e552a4c8266282a
Beyzaktas <134061187+Beyzakttas@users.noreply.github.com>
2024-02-03 18:02:29 +0300

    Add project files.

---
+++ DOSYA İSLEMLERİ/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace DOSYA_İSLEMLERİ
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnklasorolustur = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnklasörsil = new System.Windows.Forms.Button();
+            this.btnyolkontrol = new System.Windows.Forms.Button();
+            this.btnolusturmatarihi = new System.Windows.Forms.Button();
+            this.aktifprogramyolu = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnsonerisimzamani = new System.Windows.Forms.Button();
+            this.btnklasoradidegistir = new System.Windows.Forms.Button();
+            this.btnsuruculerlistele = new System.Windows.Forms.Button();
+            this.btnustdizin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnklasorolustur
+            // 
+            this.btnklasorolustur.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnklasorolustur.Location = new System.Drawing.Point(12, 12);
+            this.btnklasorolustur.Name = "btnklasorolustur";
+            this.btnklasorolustur.Size = new System.Drawing.Size(168, 39);
+            this.btnklasorolustur.TabIndex = 0;
+            this.btnklasorolustur.Text = "KLASÖR OLUSTUR";
+            this.btnklasorolustur.UseVisualStyleBackColor = true;
+            this.btnklasorolustur.Click += new System.EventHandler(this.btnklasorolustur_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(260, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "klasör adı:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(378, 34);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(248, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // btnklasörsil
+            // 
+            this.btnklasörsil.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnklasörsil.Location = new System.Drawing.Point(12, 57);
+            this.btnklasörsil.Name = "btnklasörsil";
+            this.btnklasörsil.Size = new System.Drawing.Size(168, 39);
+            this.btnklasörsil.TabIndex = 3;
+            this.btnklasörsil.Text = "KLASÖR SİL";
+            this.btnklasörsil.UseVisualStyleBackColor = true;
+            this.btnklasörsil.Click += new System.EventHandler(this.btnklasörsil_Click);
+            // 
+            // btnyolkontrol
+            // 
+            this.btnyolkontrol.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnyolkontrol.Location = new System.Drawing.Point(12, 102);
+            this.btnyolkontrol.Name = "btnyolkontrol";
+            this.btnyolkontrol.Size = new System.Drawing.Size(168, 39);
+            this.btnyolkontrol.TabIndex = 4;
+            this.btnyolkontrol.Text = "klasor varmi";
+            this.btnyolkontrol.UseVisualStyleBackColor = true;
+            this.btnyolkontrol.Click += new System.EventHandler(this.btnyolkontrol_Click);
+            // 
+            // btnolusturmatarihi
+            // 
+            this.btnolusturmatarihi.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnolusturmatarihi.Location = new System.Drawing.Point(12, 147);
+            this.btnolusturmatarihi.Name = "btnolusturmatarihi";
+            this.btnolusturmatarihi.Size = new System.Drawing.Size(168, 39);
+            this.btnolusturmatarihi.TabIndex = 5;
+            this.btnolusturmatarihi.Text = "olusturma tarihi";
+            this.btnolusturmatarihi.UseVisualStyleBackColor = true;
+            this.btnolusturmatarihi.Click += new System.EventHandler(this.btnolusturmatarihi_Click);
+            // 
+            // aktifprogramyolu
+            // 
+            this.aktifprogramyolu.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.aktifprogramyolu.Location = new System.Drawing.Point(12, 192);
+            this.aktifprogramyolu.Name = "aktifprogramyolu";
+            this.aktifprogramyolu.Size = new System.Drawing.Size(168, 39);
+            this.aktifprogramyolu.TabIndex = 6;
+            this.aktifprogramyolu.Text = "aktif program yolu";
+            this.aktifprogramyolu.UseVisualStyleBackColor = true;
+            this.aktifprogramyolu.Click += new System.EventHandler(this.aktifprogramyolu_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(12, 237);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(168, 39);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "program file list";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(330, 115);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(351, 292);
+            this.listBox1.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(420, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(144, 20);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "listeleme veriler";
+            // 
+            // btnsonerisimzamani
+            // 
+            this.btnsonerisimzamani.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnsonerisimzamani.Location = new System.Drawing.Point(12, 282);
+            this.btnsonerisimzamani.Name = "btnsonerisimzamani";
+            this.btnsonerisimzamani.Size = new System.Drawing.Size(168, 39);
+            this.btnsonerisimzamani.TabIndex = 10;
+            this.btnsonerisimzamani.Text = "son ersisim zaman";
+            this.btnsonerisimzamani.UseVisualStyleBackColor = true;
+            this.btnsonerisimzamani.Click += new System.EventHandler(this.btnsonerisimzamani_Click);
+            // 
+            // btnklasoradidegistir
+            // 
+            this.btnklasoradidegistir.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnklasoradidegistir.Location = new System.Drawing.Point(12, 327);
+            this.btnklasoradidegistir.Name = "btnklasoradidegistir";
+            this.btnklasoradidegistir.Size = new System.Drawing.Size(168, 39);
+            this.btnklasoradidegistir.TabIndex = 11;
+            this.btnklasoradidegistir.Text = "klasor ad degistir";
+            this.btnklasoradidegistir.UseVisualStyleBackColor = true;
+            this.btnklasoradidegistir.Click += new System.EventHandler(this.btnklasoradidegistir_Click);
+            // 
+            // btnsuruculerlistele
+            // 
+            this.btnsuruculerlistele.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnsuruculerlistele.Location = new System.Drawing.Point(12, 372);
+            this.btnsuruculerlistele.Name = "btnsuruculerlistele";
+            this.btnsuruculerlistele.Size = new System.Drawing.Size(168, 39);
+            this.btnsuruculerlistele.TabIndex = 12;
+            this.btnsuruculerlistele.Text = "suruculer";
+            this.btnsuruculerlistele.UseVisualStyleBackColor = true;
+            this.btnsuruculerlistele.Click += new System.EventHandler(this.btnsuruculerlistele_Click);
+            // 
+            // btnustdizin
+            // 
+            this.btnustdizin.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnustdizin.Location = new System.Drawing.Point(12, 417);
+            this.btnustdizin.Name = "btnustdizin";
+            this.btnustdizin.Size = new System.Drawing.Size(168, 39);
+            this.btnustdizin.TabIndex = 13;
+            this.btnustdizin.Text = "ust dizin";
+            this.btnustdizin.UseVisualStyleBackColor = true;
+            this.btnustdizin.Click += new System.EventHandler(this.btnustdizin_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnustdizin);
+            this.Controls.Add(this.btnsuruculerlistele);
+            this.Controls.Add(this.btnklasoradidegistir);
+            this.Controls.Add(this.btnsonerisimzamani);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.aktifprogramyolu);
+            this.Controls.Add(this.btnolusturmatarihi);
+            this.Controls.Add(this.btnyolkontrol);
+            this.Controls.Add(this.btnklasörsil);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnklasorolustur);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnklasorolustur;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnklasörsil;
+        private System.Windows.Forms.Button btnyolkontrol;
+        private System.Windows.Forms.Button btnolusturmatarihi;
+        private System.Windows.Forms.Button aktifprogramyolu;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnsonerisimzamani;
+        private System.Windows.Forms.Button btnklasoradidegistir;
+        private System.Windows.Forms.Button btnsuruculerlistele;
+        private System.Windows.Forms.Button btnustdizin;
+    }
+}
+
---
+++ DOSYA İSLEMLERİ/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;//kloasor olusturmak ıcın kutuphanae tanımla
+
+
+namespace DOSYA_İSLEMLERİ
+{
+    public partial class Form1 : Form
+    {
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnklasorolustur_Click(object sender, EventArgs e)
+        {
+            string klasor = textBox1.Text;
+         Directory.CreateDirectory   ("C:\\Users\\beyza aktas\\OneDrive\\Belgeler\\C#\\c# form app\\DOSYA İSLEMLERİ\\DOSYA İSLEMLERİ\\bin" + klasor );// dosya ekleme
+        }
+
+        private void btnklasörsil_Click(object sender, EventArgs e)
+        {
+            string klasor = textBox1.Text;
+            Directory.Delete("C:\\Users\\beyza aktas\\OneDrive\\Belgeler\\C#\\c# form app\\DOSYA İSLEMLERİ\\DOSYA İSLEMLERİ\\bin" + klasor);// dosya silme istenilen dosyanın ozellikler guvenlıge gel name koppyala
+        }
+
+        private void btnyolkontrol_Click(object sender, EventArgs e)
+        {
+            string yol = @"C:\Users\beyza aktas\OneDrive\Belgeler\C#\c# form app\DOSYA İSLEMLERİ\DOSYA İSLEMLERİ\bin";// dosya var mi yok mu true folse
+            MessageBox.Show(Directory.Exists(yol).ToString());
+        }
+
+        private void btnolusturmatarihi_Click(object sender, EventArgs e)
+        {
+            string adres = @"C:\Users\beyza aktas\OneDrive\Belgeler\C#\c# form app\DOSYA İSLEMLERİ\DOSYA İSLEMLERİ\bin";
+            MessageBox.Show(Directory.GetCreationTime(adres).ToString ());// adresin olusturma zamanını al
+        }
+
+        private void aktifprogramyolu_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(Directory.GetCurrentDirectory().ToString());
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label2.Text = "program dosyalari";
+
+            string adres = @"C:\Users\beyza aktas\OneDrive\Belgeler\C#\c# form app";
+            string[] klasor = Directory.GetDirectories(adres);
+            foreach (string i in klasor)
+            {
+                listBox1.Items.Add(ToString());
+            }
+        }
+
+        private void btnsonerisimzamani_Click(object sender, EventArgs e)
+        {
+            string yol = @"C:\Users\beyza aktas\OneDrive\Belgeler\C#\c# form app\DOSYA İSLEMLERİ\DOSYA İSLEMLERİbeyza";
+            MessageBox.Show(Directory.GetLastAccessTime(yol).ToString());
+        }
+
+        private void btnklasoradidegistir_Click(object sender, EventArgs e)
+        {
+            Directory.Move(@"C:\Users\beyza aktas\OneDrive\Belgeler\C#\c# form app\DOSYA İSLEMLERİ\DOSYA İSLEMLERİbeyza", @"C:\Users\beyza aktas\OneDrive\Belgeler\C#\c# form app\DOSYA İSLEMLERİ\ayse");
+        }
+
+        private void btnsuruculerlistele_Click(object sender, EventArgs e)
+        {
+string[]dizi=System.IO.Directory.GetLogicalDrives();
+            listBox1.Items.AddRange(dizi);
+        }
+
+        private void btnustdizin_Click(object sender, EventArgs e)
+        {
+            DirectoryInfo ustdizin = Directory.GetParent(@"C:\Users\beyza aktas\OneDrive\Resimler");
+            MessageBox.Show(ustdizin.ToString());
+        }
+    }
+}
